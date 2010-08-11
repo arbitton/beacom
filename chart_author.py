bb8c030b1c36b91af359331eafea4897119652f6
Ayelet Bitton <ayelet@ucsd.edu>
2010-08-11 22:17:55 +0200

    Finished beacom code -- testing and debugging left

--- chart_author.py
+++ chart_author.py
@@ -12,10 +12,10 @@ year_re = re.compile("^[0-9]{4}")
 
 def find_cites(author):
 
-   time = 0
-
+   print "# Author:", author
    papers = get_realauthor_data(author, 'bibrec_id')
    
+   start_year = 0
    year_dict = {}
 
    for i in range(0, len(papers)):
@@ -29,6 +29,19 @@ def find_cites(author):
             year_dict[int(year.group())] += 1
          # print year.group()
 
+   if len(year_dict) > 0:
+
+      start_year = min(year_dict.keys())
+      end_year = max(year_dict.keys())
+
+      for i in range(start_year, end_year):
+         if i not in year_dict:
+            year_dict[i] = 0
+
+   else:
+
+      print "# Author has no citations"
+
    print year_dict
 
    #pull all cited papers of author's papers
@@ -37,25 +50,39 @@ def find_cites(author):
    #(make sure to start time at date of first paper published!)
    #print out points
 
-   return year_dict
+   return year_dict, start_year
 
 def plot_points(year_dict, start_year):
 
+   current_year = start_year
    start = True
+   total_cites = 0 
 
    for year in year_dict:
-      # this is wrong, fix this
-      if start:
-         start_year = year
-         start = False
+      total_cites += year_dict[current_year]
+      # print (cites in last five years), (total cites)
+      print find_citeslast5years(year_dict, start_year, current_year), total_cites 
+      current_year += 1
 
-      #calculate some stuff plx 
+def find_citeslast5years(year_dict, start_year, current_year):
+
+   result = 0
+
+   if current_year - start_year >= 4:
+      for i in range(0, 5):
+         result += year_dict[current_year - i]
+   else:
+      while start_year <= current_year:
+         result += year_dict[start_year]
+         start_year += 1
+
+   return result
 
 def main(authors):
 
    for author in authors:
-      year_dict = find_cites(author)
-      plot_points(year_dict)
+      year_dict, start_year = find_cites(author)
+      plot_points(year_dict, start_year)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
